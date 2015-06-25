693087da4757b292e59852b35aaafa0990b40a6e
wmuyoma16 <wmuyoma16@my.whitworth.edu>
2015-06-25 16:03:45 -0700

    all the crap worked on

---
+++ WebApplication1/Scripts/scrolling.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $(function () {
+        $(window).on("load resize", function () {
+            $(".fill-screen").css("height", window.innerHeight);
+        });
+    });
+    //scroll spy
+    $("body").scrollspy({
+        target:".navbar",
+        offset:160
+    });
+    $('nav a, .click-down a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top-70
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    $(window).stellar();
+});
